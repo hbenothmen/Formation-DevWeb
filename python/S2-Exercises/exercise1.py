4eb120b6990418b69935d932e9a084bf6dae5f28
hbenothmen <benothmenhafiza@gmail.com>
2026-06-13 18:29:12 +0100

    b2 modif

--- python/S2-Exercises/exercise1.py
+++ python/S2-Exercises/exercise1.py
@@ -2,8 +2,9 @@
 print("Hello World")
 a=3
 b=2.5
-c="bonjour"
+c="bonjour hafiza"
 d='True'
+e='False'
 print("a est de type:", type(a))
 print("b est de type:", type(b))
 print("c est de type:", type(c))
